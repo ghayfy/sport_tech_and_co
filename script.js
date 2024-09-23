64c33f2335e4a6d313f42c9b45a573d1cd3069c3
ghayfy <75206538+ghayfy@users.noreply.github.com>
2024-09-23 13:30:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll pour les liens du menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
